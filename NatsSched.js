d438bf852e0461b2819e2dc008101a3de8f79f4b
aheyman11 <aheyman11@gmail.com>
2016-05-02 12:46:18 -0400

    Added html form selector for each team. On selection, sends appropriate API request.

--- NatsSched.js
+++ NatsSched.js
@@ -3,7 +3,7 @@
 const MAX_RESULTS = 10;
 var responseJSON;
 
-// Creates new XMLHttpRequest object to handle requests to backend server.
+// Creates new XMLHttpRequest object to send requests to backend server.
 var xhttp;
 if (window.XMLHttpRequest) {
     xhttp = new XMLHttpRequest();
@@ -12,9 +12,6 @@ else {
     // code for IE6, IE5
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
-
-xhttp.open("GET", "http://127.0.0.1:8081/");
-xhttp.send();
 
 // Wait until response has been received, then handle text.
 xhttp.onreadystatechange = function() {
@@ -25,11 +22,17 @@ xhttp.onreadystatechange = function() {
 
 // Helper function to parse and display results.
 var handleResponseText = function(text) {
-	//console.log(text);
-	var text_JSON = JSON.parse(text);
+	var text_JSON = JSON.parse(text); // object from JSON text
+	var table_html = "";
 	for (var i = 0; i < MAX_RESULTS; i++) {
 		var datetime = new Date(text_JSON.events[i].datetime_utc);
 		var nextEntry = "<tr> <td>" + text_JSON.events[i].title + "</td> <td>" + datetime.toDateString() + "</td> <td>" + datetime.toTimeString() + "</td> <td> $" + text_JSON.events[i].stats.average_price + "</td> </tr>";
-		$('#sched').append(nextEntry);
+		table_html += nextEntry;
 	}
+	$('#sched').html(table_html);
+}
+
+var handleTeam = function(team) {
+	xhttp.open("GET", "http://127.0.0.1:8081/" + team);
+	xhttp.send();
 }
--- backend.js
+++ backend.js
@@ -8,28 +8,31 @@ http.createServer(function (request, response) {
 	// HTTP Status: 200 : OK
 	// Content Type: text/plain
 	response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
-	makeAPICall(response);
+	var team = request.url.substring(1); // remove leading '/' from request url
+	makeAPICall(team, response);
 }).listen(8081);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
 
-var makeAPICall = function(response) {
-	var db_response = "";
+// Makes call to SeatGeek API and sends response back to frontend
+var makeAPICall = function(team, response) {
+	var db_response = []; // Buffer to store response from database (SeatGeek) server
+
 	var options = {
 		host: 'api.seatgeek.com',
-		path: '/2/events?performers.slug=washington-nationals&format=json',
+		path: '/2/events?performers.slug=' + team + '&format=json',
 		method: 'GET'
 	}
 	https.request(options, function(res) {
 		console.log("API call made\n");
 		// Read response from Seatgeek server chunk by chunk and append to db_response
 		res.on('data', function(chunk) {
-			db_response += chunk;
+			db_response.push(chunk);
 		});
 		// When all data has been received, send to frontend
 		res.on('end', function() {
-			response.end(db_response);
+			response.end(Buffer.concat(db_response).toString());
 		});
 	}).end();
 }
